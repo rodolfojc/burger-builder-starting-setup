3790ca46f25da4f1ef6889fd94796cbf72f3f4a9
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-06 19:48:14 +0000

    More about Responsive Adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -11,7 +11,7 @@ const toolbar = ( props ) => (
             <Logo />
         {/* <Logo height="80%"/> */}
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
